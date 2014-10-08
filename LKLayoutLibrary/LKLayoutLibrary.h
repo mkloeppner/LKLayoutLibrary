e0d9893fc547d782d0be04d1e9a24048e72ef42b
Martin Kloeppner <martin.kloeppner@innogames.de>
2014-10-08 19:52:49 +0200

    Initial Commit

---
+++ LKLayoutLibrary/LKLayoutLibrary.h
@@ -0,0 +1,13 @@
+//
+//  LKLayoutLibrary.h
+//  LKLayoutLibrary
+//
+//  Created by Martin Klöppner on 08/10/14.
+//  Copyright (c) 2014 mkloeppner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LKLayoutLibrary : NSObject
+
+@end
---
+++ LKLayoutLibrary/LKLayoutLibrary.m
@@ -0,0 +1,13 @@
+//
+//  LKLayoutLibrary.m
+//  LKLayoutLibrary
+//
+//  Created by Martin Klöppner on 08/10/14.
+//  Copyright (c) 2014 mkloeppner. All rights reserved.
+//
+
+#import "LKLayoutLibrary.h"
+
+@implementation LKLayoutLibrary
+
+@end
